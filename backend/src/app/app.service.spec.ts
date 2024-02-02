a3e34daa606144843dd44e8315dc0525237ce73d
SailorStat <77109932+SailorStat@users.noreply.github.com>
2024-02-02 12:46:08 +0300

    remove excess changes

--- backend/src/app/app.service.spec.ts
+++ backend/src/app/app.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 
-import AppService from "./app.service";
+import AppService from "./app.service.js";
 
 describe("AppService", () => {
   let service: AppService;
